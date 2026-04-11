4fa635dd8038e6239981788f878d3654f5ce60a4
MGhiremath0281 <muttuh028@gmail.com>
2026-04-11 18:43:21 +0530

    feat: Setup of alert service completed

---
+++ alertservice/src/test/java/com/incidentbbrain/alertservice/AlertserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incidentbbrain.alertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
