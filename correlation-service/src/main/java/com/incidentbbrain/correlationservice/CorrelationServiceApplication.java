5e98f163bc6d1dd248c2781d3a766e09f2bc2fad
MGhiremath0281 <muttuh028@gmail.com>
2026-04-13 11:20:45 +0530

    feat: Setup of correlation-service completed

---
+++ correlation-service/src/main/java/com/incidentbbrain/correlationservice/CorrelationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.incidentbbrain.correlationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrelationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrelationServiceApplication.class, args);
+    }
+
+}
---
+++ correlation-service/src/test/java/com/incidentbbrain/correlationservice/CorrelationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incidentbbrain.correlationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrelationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
