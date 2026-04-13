2cbca7a63e66e3f990e6b95fa5d4fec0c3b6d09a
MGhiremath0281 <muttuh028@gmail.com>
2026-04-13 11:46:38 +0530

    feat: define AlertEvent and IncidentEvent for Kafka communication

--- correlation-service/src/main/java/com/incidentbbrain/correlationservice/kafka/event/AlertEvent.java
+++ correlation-service/src/main/java/com/incidentbbrain/correlationservice/kafka/event/AlertEvent.java
@@ -1,4 +1,17 @@
 package com.incidentbbrain.correlationservice.kafka.event;
 
+import com.incidentbbrain.correlationservice.entity.Severity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class AlertEvent {
+    private UUID id;
+    private String serviceName;
+    private Severity severity;
+    private LocalDateTime timestamp;
 }
--- correlation-service/src/main/java/com/incidentbbrain/correlationservice/kafka/event/IncidentEvent.java
+++ correlation-service/src/main/java/com/incidentbbrain/correlationservice/kafka/event/IncidentEvent.java
@@ -1,4 +1,19 @@
 package com.incidentbbrain.correlationservice.kafka.event;
 
-public record IncidentEvent() {
+import com.incidentbbrain.correlationservice.entity.Severity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class IncidentEvent {
+    private UUID id;
+    private String service;
+    private Severity severity;
+    private List<UUID> alertIds;
 }
