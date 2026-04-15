b1c6351b836a662cd5837e6cb59b69a6faae2695
MGhiremath0281 <muttuh028@gmail.com>
2026-04-16 01:10:04 +0530

    feat: introduce metric and alert data models

--- alertservice/src/main/java/com/incidentbbrain/alertservice/dto/AlertRequest.java
+++ alertservice/src/main/java/com/incidentbbrain/alertservice/dto/AlertRequest.java
@@ -1,16 +1,13 @@
 package com.incidentbbrain.alertservice.dto;
 
-import com.incidentbbrain.alertservice.enums.Severity;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AlertRequest {
-    @NotBlank
     private String serviceName;
-    @NotNull
-    private Severity severity;
-    @NotBlank private String message;
+    private String severity;
+    private String message;
     private String host;
 }
--- alertservice/src/main/java/com/incidentbbrain/alertservice/dto/MetricPoint.java
+++ alertservice/src/main/java/com/incidentbbrain/alertservice/dto/MetricPoint.java
@@ -1,4 +1,12 @@
 package com.incidentbbrain.alertservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class MetricPoint {
+    private String name;
+    private double value;
+    private String labels;
 }
