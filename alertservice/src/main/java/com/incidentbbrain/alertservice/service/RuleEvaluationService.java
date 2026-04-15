c75dee13dbb3947f0777b0fa013ab4f34b536c58
MGhiremath0281 <muttuh028@gmail.com>
2026-04-16 01:11:04 +0530

    feat: introduce rule evaluation engine

--- alertservice/src/main/java/com/incidentbbrain/alertservice/service/RuleEvaluationService.java
+++ alertservice/src/main/java/com/incidentbbrain/alertservice/service/RuleEvaluationService.java
@@ -1,4 +1,47 @@
 package com.incidentbbrain.alertservice.service;
 
+import com.incidentbbrain.alertservice.dto.AlertRequest;
+import com.incidentbbrain.alertservice.dto.MetricPoint;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RuleEvaluationService {
+
+    public AlertRequest evaluate(List<MetricPoint> metrics) {
+
+        double cpu = 0;
+        double requests = 0;
+
+        for (MetricPoint m : metrics) {
+
+            if (m.getName().contains("process_cpu_usage")) {
+                cpu = m.getValue();
+            }
+
+            if (m.getName().contains("http_server_requests_seconds_count")) {
+                requests += m.getValue();
+            }
+        }
+
+        if (cpu > 0.7) {
+            return AlertRequest.builder()
+                    .serviceName("testing-service")
+                    .severity("HIGH")
+                    .message("CPU usage high: " + cpu)
+                    .host("metrics-engine")
+                    .build();
+        }
+
+        if (requests > 10) {
+            return AlertRequest.builder()
+                    .serviceName("testing-service")
+                    .severity("MEDIUM")
+                    .message("High request load detected: " + requests)
+                    .build();
+        }
+
+        return null;
+    }
 }
