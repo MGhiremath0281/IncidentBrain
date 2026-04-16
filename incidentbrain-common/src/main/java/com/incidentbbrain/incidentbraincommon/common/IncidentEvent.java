e5f58ae14b5f07623c687aa056d792947022ffe7
MGhiremath0281 <muttuh028@gmail.com>
2026-04-16 12:16:14 +0530

    fix: add JsonProperty and format for startedAt in IncidentEvent

--- incidentbrain-common/src/main/java/com/incidentbbrain/incidentbraincommon/common/IncidentEvent.java
+++ incidentbrain-common/src/main/java/com/incidentbbrain/incidentbraincommon/common/IncidentEvent.java
@@ -1,5 +1,7 @@
 package com.incidentbbrain.incidentbraincommon.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -18,9 +20,13 @@ public class IncidentEvent {
     private Severity severity;
     private String status;
     private List<UUID> alertIds;
-
     private String title;
 
+    @JsonProperty("started_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime startedAt;
+
+    @JsonProperty("resolved_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime resolvedAt;
 }
