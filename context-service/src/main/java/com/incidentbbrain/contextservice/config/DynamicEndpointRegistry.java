a41edcbf7d4b96871dbccf45ce91ede2bf836570
MGhiremath0281 <muttuh028@gmail.com>
2026-04-16 11:12:48 +0530

    feat: add dynamic endpoint registry and config controller

--- context-service/src/main/java/com/incidentbbrain/contextservice/config/DynamicEndpointRegistry.java
+++ context-service/src/main/java/com/incidentbbrain/contextservice/config/DynamicEndpointRegistry.java
@@ -1,4 +1,12 @@
 package com.incidentbbrain.contextservice.config;
 
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter @Setter
 public class DynamicEndpointRegistry {
+    private String elasticsearchUrl = "http://localhost:9200/logs-*/_search";
+    private String actuatorTemplate = "http://%s:8084/actuator";
 }
--- context-service/src/main/java/com/incidentbbrain/contextservice/controller/ConfigController.java
+++ context-service/src/main/java/com/incidentbbrain/contextservice/controller/ConfigController.java
@@ -1,4 +1,20 @@
 package com.incidentbbrain.contextservice.controller;
 
-public class ConfigControlle {
+import com.incidentbbrain.contextservice.config.DynamicEndpointRegistry;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/config")
+public class ConfigController {
+    private final DynamicEndpointRegistry registry;
+
+    public ConfigController(DynamicEndpointRegistry registry) {
+        this.registry = registry;
+    }
+
+    @PostMapping("/endpoints")
+    public void updateEndpoints(@RequestParam String esUrl, @RequestParam String metricsTemplate) {
+        registry.setElasticsearchUrl(esUrl);
+        registry.setActuatorTemplate(metricsTemplate);
+    }
 }
