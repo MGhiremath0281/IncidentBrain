42313b608b28687682b8272faec58b59de066104
MGhiremath0281 <muttuh028@gmail.com>
2026-04-19 17:20:03 +0530

    feat: configure kafka consumer and incoming event dto

--- jira-service/src/main/java/com/incidentbbrain/jiraservice/config/KafkaConfig.java
+++ jira-service/src/main/java/com/incidentbbrain/jiraservice/config/KafkaConfig.java
@@ -1,4 +1,28 @@
 package com.incidentbbrain.jiraservice.config;
 
+import com.incidentbbrain.jiraservice.dto.AnalysisEvent;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.Map;
+
+@Configuration
 public class KafkaConfig {
+    @Bean
+    public ConsumerFactory<String, AnalysisEvent> consumerFactory(KafkaProperties props) {
+        JsonDeserializer<AnalysisEvent> jsonDeserializer = new JsonDeserializer<>(AnalysisEvent.class);
+        jsonDeserializer.addTrustedPackages("com.incidentbbrain.jiraservice.dto");
+
+        return new DefaultKafkaConsumerFactory<>(
+                props.buildConsumerProperties(null),
+                new StringDeserializer(),
+                new ErrorHandlingDeserializer<>(jsonDeserializer)
+        );
+    }
 }
